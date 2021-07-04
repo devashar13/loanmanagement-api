187cf71dc0252c8c73454074a5457996a4529272
Dev Ashar <devashar13@gmail.com>
2021-07-04 13:34:24 +0530

    inital commit

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+
+)
+urlpatterns = [
+    path('login', TokenObtainPairView.as_view(), name='login'),
+]
